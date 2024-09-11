a6e41d14a96d20d777ed5deea291104a3ae30e06
vizaaya <zahrafita441@gmail.com>
2024-09-11 13:33:00 +0700

    edit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:14
-LABEL org.opencontainers.image.source="https://github.com/vizaaya/a433-microservices"
+
 WORKDIR /app
 COPY . .
 ENV NODE_ENV=production 
--- build_push_image.sh
+++ build_push_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 docker build -t item-app:v1 .
 docker images
-docker tag item-app:v1 ghcr.io/vizaaya/item-app:v1
-docker login ghcr.io
-docker push ghcr.io/vizaaya/item-app:v1
+docker tag item-app:v1 vizaa/item-app:v1
+echo $PASSWORD | docker login -u vizaa --password-stdin
+docker push vizaa/item-app:v1
